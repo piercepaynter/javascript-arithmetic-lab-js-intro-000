427fb267318ee3cb1903f6ad43d11c7f921703d3
boisterous-puppet-2200 <paynterpierce@gmail.com>
2020-06-18 05:07:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ function add(a, b){
   return a + b;
 }
 
-function (a, b) {
+function (a, b){
   return a - b;
 }
 
 function multiply(a, b){
-  return a*b;
+  return a * b;
 }
 
-function divide(a, b) {
-  return a/b;
+function divide(a, b){
+  return a / b;
 }
